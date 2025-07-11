46888c3072976395d49869aafe3b7f7b0cdb54cc
kunal <monupanwar5655@gmail.com>
2025-07-11 18:17:02 +0530

    some html added

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -4,13 +4,13 @@ import { Badge } from '@/components/ui/badge';
 
 export default function page() {
   return (
-    <main className="mt-10 min-h-screen flex items-center justify-center flex-col space-y-8 ">
+    <main className="mt-10 min-h-screen flex items-center justify-center flex-col space-y-10 ">
       <section className="container mx-auto max-w-4xl text-center">
         <Badge variant="secondary" className="mb-4">
           🤖 Powered by AI · Built for Creators
         </Badge>
         <h1 className="text-4xl md:text-6xl font-bold mb-6 bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 dark:from-white dark:via-gray-100 dark:to-white bg-clip-text text-transparent">
-          AI-Generated
+          AI-Generated Profile
           <br />
           <span className="bg-gradient-to-r from-indigo-500 via-purple-600 to-pink-500 bg-clip-text text-transparent">
             Start Gerenrating now
